c713294802896ca93e020d8c505344376e4db983
nyradr <nyradr@protonmail.com>
2016-09-29 20:32:24 +0200

    utils to pdo

--- serv/trrk/watch/utils.php
+++ serv/trrk/watch/utils.php
@@ -5,11 +5,6 @@ define("DB", "db");
 define("USR", "usr");
 define("MDP", "mdp");
 
-function connect(){
-    mysql_connect(HOST, USR, MDP) or die("CONNECT : " . mysql_error());
-	mysql_select_db(DB) or die("select db : " . mysql_error());
-}
-
 function coPDO(){
 	$GLOBALS['db'] = new PDO("mysql:host=" . HOST . ";dbname="  .DB, USR, MDP);
 	
@@ -24,10 +19,13 @@ function spost($id){
  * 	return null if fb id not found
 */
 function getPersonId($fbid){
-    $query = "SELECT id FROM Pers WHERE idstr='$fbid'";
-    $ret = mysql_query($query);
+    $query = "SELECT id FROM Pers WHERE idstr= :id";
+    $stmt = $GLOABLS['db']->prepare($sql);
+    $stmt->bindParam(":id", $fbid);
     
-    if($e = mysql_fetch_assoc($ret))
+    $stmt->execute();
+    
+    if($e = $stmt->fetch(PDO::FETCH_ASSOC))
 		return $e['id'];
 	
     return null;
@@ -58,20 +56,26 @@ function isPersonId($id){
 }
 
 function getPlaceId($fbid){
-	$query = "SELECT id FROM Place WHERE pname='$fbid'";
-    $ret = mysql_query($query);
+	$query = "SELECT id FROM Place WHERE pname= :id";
+    $stmt = $GLOBALS['db']->prepare($sql);
+    $stmt->bindParam(":id", $fbid);
     
-    if($e = mysql_fetch_assoc($ret))
+    $stmt->execute();
+    
+    if($e = $stmt->fetch(PDO::FETCH_ASSOC))
 		return $e['id'];
 	
     return null;
 }
 
 function getWorkId($fbid){
-	$query = "SELECT id FROM Workplace WHERE wname='$fbid'";
-    $ret = mysql_query($query);
+	$query = "SELECT id FROM Workplace WHERE wname= :id";
+    $stmt = $GLOBALS['db']->prepare($sql);
+    $stmt->bindParam(":id", $fbid);
     
-    if($e = mysql_fetch_assoc($ret))
+    $stmt->execute();
+    
+    if($e = $stmt->fetch(PDO::FETCH_ASSOC))
 		return $e['id'];
 	
     return null;
@@ -79,10 +83,13 @@ function getWorkId($fbid){
 
 function getImgId($url){
 	
-	$query = "SELECT id FROM Photo WHERE img='$url'";
-	$ret = mysql_query($query);
+	$query = "SELECT id FROM Photo WHERE img= :url";
+	$stmt = $GLOBALS['db']->prepare($sql);
+	$stmt->bindParam(":url", $url);
 	
-	if($e = mysql_fetch_assoc($ret))
+	$stmt->execute();
+	
+	if($e = $stmt->fetch(PDO::FETCH_ASSOC))
 		return $e["id"];
 		
 	return null;
