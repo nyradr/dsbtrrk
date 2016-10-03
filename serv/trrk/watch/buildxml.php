667dde48bdbbe4d0b813179c736b344aa19029ef
nyradr <nyradr@protonmail.com>
2016-10-03 18:58:11 +0200

    buildxml to pdo

--- serv/trrk/watch/buildxml.php
+++ serv/trrk/watch/buildxml.php
@@ -2,32 +2,28 @@
 
 function buildLive($id){
     $query_live = "SELECT idplace FROM Live WHERE idpers='$id'";
-    $ret = mysql_query($query_live);
+    $ret = $GLOBALS['db']->query($query_live);
     
     echo "<live>";
     
-    while($e = mysql_fetch_assoc($ret)){
+    while($e = $ret->fetch(PDO::FETCH_ASSOC)){
 	echo "<idplace rid='" .$e["idplace"] . "' />";
     }
     
     echo "</live>";
-    
-    mysql_free_result($ret);
 }
 
 function buildWorkplace($id){
     $query_work = "SELECT idwork FROM Workat WHERE idpers='$id'";
-    $ret = mysql_query($query_work);
+    $ret = $GLOBALS['db']->query($query_work);
     
     echo "<workat>";
     
-    while ($e = mysql_fetch_assoc($ret)){
+    while ($e = $ret->fetch(PDO::FETCH_ASSOC)){
 	echo "<idwork rid='" .$e["idwork"]. "' />";
     }
     
     echo "</workat>";
-    
-    mysql_free_result($ret);
 }
 
 function buildPers($pers){
@@ -48,51 +44,45 @@ function buildPers($pers){
 
 function buildPersons(){
     $query_pers = "SELECT * FROM Pers";
-    $ret = mysql_query($query_pers) or die("HERE");
+    $ret = $GLOBALS['db']->query($query_pers) or die("HERE");
 
     
     echo "<Persons>";
     
-    while($e = mysql_fetch_assoc($ret))
+    while($e = $ret->fetch(PDO::FETCH_ASSOC))
 	buildPers($e);
-    
-    mysql_free_result($ret);
     
     echo "</Persons>";
 }
 
 function buildFriend(){
     $query_friend = "SELECT * FROM Friend";
-    $ret = mysql_query($query_friend);
+    $ret = $GLOBALS['db']->query($query_friend);
     
     echo "<Friends>";
     
-    while($e = mysql_fetch_assoc($ret)){
+    while($e = $ret->fetch(PDO::FETCH_ASSOC)){
 	echo "<friend>";
 	echo "<id1 rid='" .$e["idpers"] ."' />";
 	echo "<id2 rid='" .$e["idfrd"] ."' />";
 	echo "</friend>";
     }
     
-    mysql_free_result($ret);
-    
     echo "</Friends>";
 }
 
 function buildPlaces(){
     $query_place = "SELECT * FROM Place";
-    $ret = mysql_query($query_place);
+    $ret = $GLOBALS['db']->query($query_place);
     
     echo "<Places>";
     
-    while ($e = mysql_fetch_assoc($ret)){
+    while ($e = $ret->fetch(PDO::FETCH_ASSOC)){
 	echo "<place uid='" .$e["id"] ."'>";
 	echo "<pname>" .$e["pname"]. "</pname>";
 	echo "<ptype>" .$e["typeent"]. "</ptype>";
 	echo "</place>";
     }
-    
-    mysql_free_result($ret);
     
     echo "</Places>";
     
@@ -100,11 +90,11 @@ function buildPlaces(){
 
 function buildWorks(){
     $query_work = "SELECT * FROM Workplace";
-    $ret = mysql_query($query_work);
+    $ret = $GLOBALS['db']->query($query_work);
     
     echo "<Workplaces>";
     
-    while($e = mysql_fetch_assoc($ret)){
+    while($e = $ret->fetch(PDO::FETCH_ASSOC)){
 	echo "<workplace id='" .$e["id"] ."' >";
 	echo "<wname>" .$e["wname"]. "</wname>";
 	echo "<wtype>" .$e["typeent"]. "</wtype>";
@@ -113,23 +103,19 @@ function buildWorks(){
     
     echo "</Workplaces>";
     
-    mysql_free_result($ret);
-    
 }
 
 function buildLike($id){
     $query_like = "SELECT pers FROM Phlike WHERE img='$id'";
-    $ret = mysql_query($query_like);
+    $ret = $GLOBALS['db']->query($query_like);
     
     echo "<likes>";
     
-    while($e = mysql_fetch_assoc($ret)){
+    while($e = $ret->fetch(PDO::FETCH_ASSOC)){
 	echo "<id rid='" .$e["pers"]. "'/>";
     }
     
     echo "</likes>";
-    
-    mysql_free_result($ret);
 }
 
 function buildPhoto($ph){
@@ -145,15 +131,13 @@ function buildPhoto($ph){
 
 function buildPhotos(){
     $query_photo = "SELECT * FROM Photo";
-    $ret = mysql_query($query_photo);
+    $ret = $GLOBALS['db']->query($query_photo);
     
     echo "<Photos>";
     
-    while($e = mysql_fetch_assoc($ret)){
+    while($e = $ret->fetch(PDO::FETCH_ASSOC)){
 		buildPhoto($e);
     }
-    
-    mysql_free_result($ret);
     
     echo "</Photos>";
 }
@@ -163,7 +147,7 @@ function buildPhotos(){
 
 include_once("utils.php");
 
-connect();
+coPDO();
 
 echo "<?xml version='1.0' encoding='UTF-8' ?>";
 echo "<Facebook>";
@@ -175,7 +159,5 @@ buildWorks();
 buildPhotos();
 
 echo "</Facebook>";
-
-mysql_close();
 
 ?>
